fba90d11cc9433a014c0cc3f55ef3f6f95604047
Derek Redfern <redfern.derek@gmail.com>
2014-01-19 11:57:34 -0500

    Fixed indentation

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ filename = "Log." + str(time.mktime(time.localtime())) + ".csv"
 
 
 with open(filename, 'wb') as csvfile:
-	IMUwriter = csv.writer(csvfile, delimiter=' ',
+    IMUwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-s = serial.Serial(port='/dev/tty.usbserial-A501JT18', baudrate=115200)
-i = 0
+    s = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
+    i = 0
 
-while 1:
-#s.write('text')
-	print "A: ", i
-	i += 1
-	readLine =  s.readline()
-	IMUwriter.writerow(readLine.split('\t'))
-	print readLine.split('\t')
+    while 1:
+    #s.write('text')
+        print "A: ", i
+        i += 1
+        readLine =  s.readline()
+        IMUwriter.writerow(readLine.split('\t'))
+        print readLine.split('\t')
